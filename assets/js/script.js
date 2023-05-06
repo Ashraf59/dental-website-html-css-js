5981333397a726fa2a49e2915fc1dee6347f3116
Ashraf59 <ashrafams59b@gmail.com>
2023-05-07 02:26:53 +0600

    Dental website with html - css - js

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+'use-strict';
+
+/**
+ * addEvent on element
+ */
+
+const addEventOnElem = function (elem, type, callback){
+    if(elem.length > 1){
+        for(let i = 0; i < elem.length; i++){
+            
+            elem[i].addEventListener(type, callback)
+        }
+    }
+    else{
+        elem.addEventListener(type, callback)
+        
+    }
+}
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggle = document.querySelector("[data-nav-toggle]");
+
+
+const toggleNav = function(){
+    navbar.classList.toggle("active");
+    navbarToggle.classList.toggle("active")
+}
+
+addEventOnElem(navbarToggle, "click", toggleNav)
+
+const closeNav = function(){
+    navbar.classList.remove("active");
+    navbarToggle.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNav)
+
+
+/**
+ * header active
+ */
+
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY >= 100){
+        header.classList.add("active");
+    }
+    else{
+        header.classList.remove("active");
+    }
+})
