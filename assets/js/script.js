ffde061d3847acfb74cd00ff60598b49dbad4f34
Ashraf59 <ashrafams59b@gmail.com>
2023-05-09 01:09:48 +0600

    Complete the dental website

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,12 +48,15 @@ addEventOnElem(navbarLinks, "click", closeNav)
 
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]")
 
 window.addEventListener("scroll", function(){
     if(window.scrollY >= 100){
         header.classList.add("active");
+        backTopBtn.classList.add("active")
     }
     else{
         header.classList.remove("active");
+        backTopBtn.classList.remove("active")
     }
 })
